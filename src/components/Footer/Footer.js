399417b863b9f06abd8f0db21fbee1e9ed4f6216
Daniel Batista <dani_batista20@hotmail.com>
2020-11-24 19:34:59 +0000

    upd:  header, footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,10 +3,12 @@ import React, { Component } from 'react';
 class Footer extends Component {
     render() {
         return (
-            <footer className="container">
-                <div className="row">
-                    <div className="col-12 text-center p-2">
-                        <p>footer</p>
+            <footer>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12 text-center p-2">
+                            <p>footer</p>
+                        </div>
                     </div>
                 </div>
             </footer>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,10 +3,12 @@ import React, { Component } from 'react';
 class Header extends Component {
     render() {
         return (
-            <header className="container">
-                <div className="row">
-                    <div className="col-12 text-center p-4">
-                        <h1>React Todo App</h1>
+            <header>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12 text-center p-4">
+                            <h1>React Todo App</h1>
+                        </div>
                     </div>
                 </div>
             </header>
