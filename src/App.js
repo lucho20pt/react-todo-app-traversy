324f683df27c849f8626ba9ff0571b722acf884e
Daniel Batista <dani_batista20@hotmail.com>
2020-11-24 14:58:00 +0000

    start App Class & add bootrap4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <div className="row">
+        <div className="col-12 text-center">
+          <h1 className="p-4">React Todo App</h1>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
