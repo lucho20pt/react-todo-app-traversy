2b6f23a64ae0ea88c8384924f03dc2598a88a445
Daniel Batista <dani_batista20@hotmail.com>
2020-11-26 12:51:58 +0000

    upd: todo-item-completed inline to checkStyleCompleted()

--- src/components/Todos/TodoItem.js
+++ src/components/Todos/TodoItem.js
@@ -3,11 +3,18 @@ import PropTypes from 'prop-types'
 
 export class TodoItem extends Component {
 
+    checkStyleCompleted = () => {
+        return(
+            this.props.todo.completed ? 
+            "todo-item-completed" : ""
+        );
+    }
+
     render() {
         return (
             <div className="todo-item | d-flex align-items-center | px-4 py-2">
                 <input type="checkbox"/>
-                <h3 className={`m-2 ${ this.props.todo.completed ? "todo-item-completed" : ""}`}> 
+                <h3 className={`m-2 ${this.checkStyleCompleted() }`}> 
                     {this.props.todo.title}
                 </h3>
             </div>
